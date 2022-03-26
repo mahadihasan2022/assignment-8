8287459880d93ad721279f99ad4d25b97fffb7e2
mahadihasan2022 <mahadihasanmim72@gmail.com>
2022-03-26 07:50:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Products from './components/Products/Products';
+import Shop from './components/Shop/Shop';
+
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Products></Products>
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import'./Shop.css'
+
+const Shop = () => {
+    const[products, setProducts] = useState([]);
+    const[cart, setCart] = useState([]);
+
+    useEffect(() =>{
+fetch('name.json')
+.then(res => res.json())
+.then(data => setProducts(data))
+    },[])
+    const handleAddToCart = (product)=>{
+        console.log(product);
+        // cart.push(product);
+        const newCart =[...cart, product];
+        setCart(newCart);
+    }
+    return (
+        <div>
+            <h1 className='container'>Buy Your Phone</h1>
+            <div className='shop-container'>
+            <div className="products-container">
+              {
+                  products.map(product=> <Product 
+                    key={product.id}
+                    product={product}
+                    handleAddToCart={handleAddToCart}
+                    ></Product>)
+              }
+            </div>
+            <div className="cart-container">
+                <h3>Selected Phones:{cart.length}</h3>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Shop;
